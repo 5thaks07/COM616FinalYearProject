257fcde3c29a2111f807c0e7677e65558f936334
5thaks07 <5thaks07@solent.ac.uk>
2024-03-07 12:44:39 +0000

    changed the footer

--- yumyumhub/client/src/components/Footer.js
+++ yumyumhub/client/src/components/Footer.js
@@ -3,17 +3,54 @@ import React from "react";
 function Footer() {
   return (
     <footer
+      className="text-center text-lg-start"
       style={{
-        textAlign: "center",
+        backgroundColor: "grey",
         padding: "10px",
-        backgroundColor: "#333",
         color: "white",
-        
         bottom: "0",
         width: "100%",
       }}
     >
-      &copy; 2024 YumYumHub
+      <div className="container d-flex justify-content-center py-5">
+        <button
+          type="button"
+          className="btn btn-primary btn-lg btn-floating mx-2"
+          style={{ backgroundColor: "#54456b" }}
+        >
+          <i className="fab fa-facebook-f"></i>
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary btn-lg btn-floating mx-2"
+          style={{ backgroundColor: "#54456b" }}
+        >
+          <i className="fab fa-youtube"></i>
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary btn-lg btn-floating mx-2"
+          style={{ backgroundColor: "#54456b" }}
+        >
+          <i className="fab fa-instagram"></i>
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary btn-lg btn-floating mx-2"
+          style={{ backgroundColor: "#54456b" }}
+        >
+          <i className="fab fa-twitter"></i>
+        </button>
+      </div>
+
+      {/* Copyright */}
+      <div
+        className="text-center text-white p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2024 YumYumHub
+      </div>
+      {/* Copyright */}
     </footer>
   );
 }
