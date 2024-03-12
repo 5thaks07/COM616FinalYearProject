d61d3b19c279d7f78afc1337e5cee85859cd7f2c
5thaks07 <5thaks07@solent.ac.uk>
2024-03-12 01:14:19 +0000

    Added models for db

---
+++ yumyumhub/server/src/models/Recipe.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from 'mongoose';
+
+const recipeSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    name: { type: String, required: true },
+    shortDescription: { type: String, required: true },
+    fullDescription: { type: String, required: true },
+    ingredients: { type: String, required: true },
+    servings: { type: Number, required: true },
+    time: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+    rating: { type: Number, default: 0 },
+    images: [{ type: String }],
+  },
+  { timestamps: true }
+);
+
+export const Recipe = mongoose.model('Recipe', recipeSchema);
---
+++ yumyumhub/server/src/models/User.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: { type: String },
+    email: {
+      type: String,
+      required: [true, 'email is required'],
+      unique: true,
+    },
+    password: { type: String, required: [true, 'password is required'] },
+
+    token: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
