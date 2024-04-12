e23f3dd36bce1fb7909532de841d69637f30d6fb
5thaks07 <5thaks07@solent.ac.uk>
2024-04-12 15:02:35 +0100

    added chat model and controllers

---
+++ server/src/controllers/chat.controller.js
@@ -0,0 +1,57 @@
+import Chat from '../models/chat.model.js';
+
+// createChat
+
+export const createChat = async (req, res) => {
+  const { firstUserId, secondUserId } = req.body;
+
+  try {
+    const testchat = await Chat.findOne({
+      members: { firstUserId, secondUserId },
+    });
+
+    if (testchat) {
+      return res.status(200).json({ testchat });
+    }
+
+    const newChat = new Chat({
+      members: [firstUserId, secondUserId],
+    });
+
+    const chat = await newChat.save();
+    res.status(201).json({ chat });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+// find user chats
+
+export const findUserChats = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const chats = await Chat.find({ members: userId });
+
+    res.status(200).json({ chats });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+//findChat
+
+export const findChat = async (req, res) => {
+  const { firstUserId, secondUserId } = req.params;
+
+  try {
+    const chat = await Chat.findOne({ members: { firstUserId, secondUserId } });
+
+    res.status(200).json({ chat });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
---
+++ server/src/models/Chat.js
@@ -0,0 +1,13 @@
+// model for a chat 
+
+import mongoose, { Schema } from 'mongoose';
+
+const chatSchema = new Schema(
+    {
+        // chat members
+        members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    },
+    { timestamps: true }
+);
+
+export const Chat = mongoose.model('Chat', chatSchema);
